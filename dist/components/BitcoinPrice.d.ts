6f41c610e3c0b432bac43184d1676ed1c4f6be0a
abe <abel.rogers@gmail.com>
2023-10-19 10:24:00 +0100

    Fist commit

---
+++ dist/components/BitcoinPrice.d.ts
@@ -0,0 +1,3 @@
+import React from 'react';
+declare const BitcoinPrice: React.FC;
+export default BitcoinPrice;
---
+++ dist/components/BitcoinPrice.js
@@ -0,0 +1,63 @@
+"use strict";
+'use client';
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const react_1 = __importStar(require("react"));
+const axios_1 = __importDefault(require("axios"));
+const API_ENDPOINT = 'https://api.coindesk.com/v1/bpi/currentprice.json';
+const BitcoinPrice = () => {
+    const [price, setPrice] = (0, react_1.useState)(null);
+    (0, react_1.useEffect)(() => {
+        const fetchPrice = () => __awaiter(void 0, void 0, void 0, function* () {
+            try {
+                const response = yield axios_1.default.get(API_ENDPOINT, {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        // other headers if needed
+                    }
+                });
+                setPrice(response.data.bpi.USD.rate_float);
+            }
+            catch (error) {
+                console.error('Error fetching Bitcoin price:', error);
+            }
+        });
+        fetchPrice();
+    }, []);
+    return (react_1.default.createElement("div", null, price ? `Current Bitcoin Price: $${price}` : 'Fetching Bitcoin price...'));
+};
+exports.default = BitcoinPrice;
